5c4ce0883b1fbe1942316899888086a61940748d
djairofilho <djairodsfilho@gmail.com>
2026-04-26 17:03:21 -0300

    feat(prolog): add named query1

--- prolog/src/queries.pl
+++ prolog/src/queries.pl
@@ -2,8 +2,22 @@
 % Carregue o projeto com:
 % ?- consult('prolog/main.pl').
 
+query1(Lista) :-
+    findall(
+        Nome,
+        (
+            recomendado_por_genero_e_plataforma(Jogo, action, windows),
+            nome(Jogo, Nome)
+        ),
+        Lista
+    ).
+
 % 1. Quais jogos de action para windows sao recomendados?
+% ?- query1(Lista).
+% Executa:
 % ?- findall(Nome, (recomendado_por_genero_e_plataforma(Jogo, action, windows), nome(Jogo, Nome)), Lista).
+% Resposta esperada:
+% Lista = ['Counter-Strike 2', 'Grand Theft Auto V Legacy', 'Team Fortress 2' | ...].
 
 % 2. Quais jogos possuem bom custo-beneficio?
 % ?- findall(Nome-Preco-Nota, (recomendado_custo_beneficio(Jogo), nome(Jogo, Nome), preco(Jogo, Preco), avaliacao(Jogo, Nota)), Lista).
